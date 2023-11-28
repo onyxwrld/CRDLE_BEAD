e917ad2e7ff9e970655823bc4b65fbd2f96a3b56
Petrik Diak <diak@petrik.hu>
2023-11-28 09:50:33 +0100

    névre keres függvény

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css';
-import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import {getAll} from "./card/cardService";
 import { card } from './card/card';
 //https://frontend-dle-db-default-rtdb.europe-west1.firebasedatabase.app/.json
@@ -13,5 +13,37 @@ const cards: card[] = [];
     }
     console.log(cards);
 }
-document.addEventListener('DOMContentLoaded', init)
+
+async function kiir(name:string)
+{
+    const a = document.getElementById('tbodyID') as HTMLInputElement;
+    const data = await getAll();
+    const DataTable = data.filter(x=>x.name === name).map((ab) =>{
+        const tr = document.createElement("tr");
+        const idCol = document.createElement("td");
+        const nameCol = document.createElement("td");
+        const arenaCol = document.createElement("td");
+        const typeCol = document.createElement("td");
+        const elixirCol = document.createElement("td");
+        const rarityCol = document.createElement("td");
+        const descriptionCol = document.createElement("td");
+        const guessedCol = document.createElement("td");
+        idCol.textContent = ab.id.toString();
+        nameCol.textContent = ab.name;
+        arenaCol.textContent = ab.arena.toString();
+        typeCol.textContent = ab.type;
+        elixirCol.textContent = ab.elixir.toString();
+        rarityCol.textContent = ab.rarity;
+        descriptionCol.textContent = ab.description;
+        tr.append(...[idCol, nameCol, arenaCol, typeCol,elixirCol,rarityCol,descriptionCol]);
+        return tr;
+    });
+    a.append(...DataTable);
+}
+document.addEventListener('DOMContentLoaded', () =>{
+    init();
+    const inputAdat = document.getElementById('inputUD') as HTMLInputElement;
+    const inputData = inputAdat.value;
+    const gombAdat = document.getElementById('gombID')!.addEventListener('click',kiir(inputData));
+})
 
