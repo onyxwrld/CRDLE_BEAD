01b2eec4649338ca98387ab2f7298eacf65ce707
Petrik Diak <diak@petrik.hu>
2023-11-28 11:19:20 +0100

    karakter kiir bugfix

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,18 @@ const cards: card[] = [];
 
 async function init() {
     const cardTable = await getAll();
-   /* for (const c of cardTable) {
+   for (const c of cardTable) {
         cards.push(new card(c.id, c.name, c.arena, c.type, c.elixir, c.rarity, c.description, c.guessed));
     }
-    console.log(cards);*/
+    console.log(cards);
     document.getElementById('gombID')?.addEventListener('click', getInputData);
 }
 
-function getInputData(){const inputData = (document.getElementById('inputUD') as HTMLInputElement).value;
-    kiir(inputData);
+function getInputData(){
+    let inputData = document.getElementById('inputID') as HTMLInputElement;
+    const a = inputData.value;
+    kiir(a);
+    inputData.value = "";
     
 }
 async function kiir(name:string)
@@ -25,22 +28,19 @@ async function kiir(name:string)
     const data = await getAll();
     const DataTable = data.filter(x=>x.name === name).map((ab) =>{
         const tr = document.createElement("tr");
-        const idCol = document.createElement("td");
         const nameCol = document.createElement("td");
         const arenaCol = document.createElement("td");
         const typeCol = document.createElement("td");
         const elixirCol = document.createElement("td");
         const rarityCol = document.createElement("td");
         const descriptionCol = document.createElement("td");
-        const guessedCol = document.createElement("td");
-        idCol.textContent = ab.id.toString();
         nameCol.textContent = ab.name;
         arenaCol.textContent = ab.arena.toString();
         typeCol.textContent = ab.type;
         elixirCol.textContent = ab.elixir.toString();
         rarityCol.textContent = ab.rarity;
         descriptionCol.textContent = ab.description;
-        tr.append(...[idCol, nameCol, arenaCol, typeCol,elixirCol,rarityCol,descriptionCol]);
+        tr.append(...[nameCol, arenaCol, typeCol,elixirCol,rarityCol,descriptionCol]);
         return tr;
     });
     a.append(...DataTable);
